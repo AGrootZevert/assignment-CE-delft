7c4b07356bfd91c6b31b8853224d419704bb434f
AGrootZevert <a.grootzevert@carbyon.com>
2026-04-07 23:15:37 +0200

    Fix visualization and main script

--- src/visualization/visualize_heat_consumption.py
+++ src/visualization/visualize_heat_consumption.py
@@ -7,7 +7,7 @@ from src.data_structures.data_structures import HeatData
 def visualize_with_average(
     hourly_data: DataFrame[HeatData],
     x_column: str,
-    y_column: str,
+    y_columns: list[str],
     x_label: str,
     y_label: str,
     title: str,
@@ -18,7 +18,7 @@ def visualize_with_average(
     Args:
         hourly_data (DataFrame[HeatData]): The hourly data
         x_column (str): the x column to plot
-        y_column (str): the y column to plot
+        y_columns (list[str]): the y columns to plot
         x_label (str): the label of the x axis
         y_label (str): the label of the y axis
         title (str): the plot title
@@ -26,39 +26,39 @@ def visualize_with_average(
     Returns:
         go.Figure: The figure
     """
-    # Calculate average heat request per hour across all days
-    hourly_avg = hourly_data[y_column].mean()
-
-    # Create figure with secondary y-axis
+    # Create figure
     fig = go.Figure()
 
-    # Add traces for each day
+    for y_column in y_columns:
+        # Calculate average heat request per hour across all days
+        hourly_avg = hourly_data[y_column].mean()
 
-    fig.add_trace(
-        go.Scatter(
-            x=hourly_data[x_column],
-            y=hourly_data[y_column],
-            mode="lines",
-            name=y_label,
-            opacity=0.3,
+        # Add traces for each day
+        fig.add_trace(
+            go.Scatter(
+                x=hourly_data[x_column],
+                y=hourly_data[y_column],
+                mode="lines",
+                name=y_column,
+                opacity=0.3,
+            )
         )
-    )
 
-    # Add average line
-    fig.add_trace(
-        go.Scatter(
-            x=hourly_data[x_column],
-            y=[hourly_avg] * len(hourly_data),
-            mode="lines",
-            name="Average",
-            line={"color": "red", "width": 3},
+        # Add average line
+        fig.add_trace(
+            go.Scatter(
+                x=hourly_data[x_column],
+                y=[hourly_avg] * len(hourly_data),
+                mode="lines",
+                name=f"Average {y_column}",
+                line={"width": 3},
+            )
         )
-    )
 
-    fig.update_layout(
-        title=title,
-        xaxis_title=x_label,
-        yaxis_title=y_label,
-        hovermode="x unified",
-    )
+        fig.update_layout(
+            title=title,
+            xaxis_title=x_label,
+            yaxis_title=y_label,
+            hovermode="x unified",
+        )
     return fig
